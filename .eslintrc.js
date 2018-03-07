1c67ace4cd366ca95eac80e0ca94f4f35954e737
Andela <andela@Andelas-MacBook-Pro.local>
2018-03-07 16:39:23 +0100

    add lint properly at last!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
     },
 };
